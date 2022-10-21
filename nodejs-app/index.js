0d4f3380af5324888ca508138298638f22916874
Carlos Cortes <krlozadan@gmail.com>
2022-10-20 21:54:49 -0700

    feat: add simple nodejs dockerized application

---
+++ nodejs-app/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const PORT = 8080;
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hi there');
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
